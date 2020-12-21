42d050c9f86a430c4645a2c5e5ea65122724936c
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-12-21 17:48:15 +0530

    Update sli_search.java

--- src/test/java/SetupClass/StepDefinition/sli_search.java
+++ src/test/java/SetupClass/StepDefinition/sli_search.java
@@ -129,7 +129,7 @@ public class sli_search extends Set {
 		Thread.sleep(4000);
 		  WebElement View_More = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#sli_autocomplete > div > div.sli_ac_products > div > div.sli_view_more_link_wrapper > a"))); 
 			Thread.sleep(3000);
-			View_More.Click();
+			View_More.click();
 			Thread.sleep(5000);
 			
 			Thread.sleep(3000);
