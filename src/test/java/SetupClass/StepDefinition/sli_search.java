1a5e819e0cdb5d17357bad68614bd8a9829fa90f
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-12-22 12:57:48 +0530

    Update sli_search.java

--- src/test/java/SetupClass/StepDefinition/sli_search.java
+++ src/test/java/SetupClass/StepDefinition/sli_search.java
@@ -92,6 +92,7 @@ public class sli_search extends Set {
 			
                         }  else {
                             System.out.println("Element is Not Visible");
+			js.executeScript("alert('Product Suggestion Box did not appeared after Keyword Removal');");
                              }
 		driver.navigate().refresh(); Thread.sleep(3800);
 
